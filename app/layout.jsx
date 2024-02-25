767168777346e0377015ec294d02c7e7bc9b0abe
PPT1001 <pragash1001@gmail.com>
2024-02-25 09:50:25 +0530

    Update CSS styles and Tailwind configuration

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,4 @@
-import { Inter } from "next/font/google";
+import { Outfit } from "next/font/google";
 import "./globals.css";
 
 //Components
@@ -7,7 +7,7 @@ import Footer from "@/components/Footer";
 //Theme Provider
 import { ThemeProvider } from "@/components/ThemeProvider";
 
-const inter = Inter({ subsets: ["latin"] });
+const outfit = Outfit({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -17,13 +17,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressContentEditableWarning>
-      <body className={inter.className}>
-        <ThemeProvider attribute = "class" defaultTheme ="light">
+      <body className={outfit.className}>
+        <ThemeProvider attribute="class" defaultTheme="light">
           <Header />
           {children}
           <Footer />
         </ThemeProvider>
-        </body>
+      </body>
     </html>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,9 +12,13 @@ module.exports = {
     container: {
       center: true,
       padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
+    },
+    screens: {
+      "2xl": "1400px",
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1400px',
     },
     extend: {
       colors: {
@@ -76,6 +80,14 @@ module.exports = {
       },
       backgroundImage: {
         hero: "url('/hero/hero-bg.png')",
+        hero_shape: "url(/hero/shape-1.svg)",
+        hero_shape_2_light: "url(/hero/shape-2-light.svg)",
+        hero_shape_2_dark: "url(/hero/shape-2-dark.svg)",
+        about_shape_light: "url(/about/shape-light.svg)",
+        about_shape_dark: "url(/about/shape-dark.svg)",
+        blend: "linear-gradient(90deg, hsl(var(--blend-from)) 0%, hsl(var(--blend-to)) 100%)",
+        dots_light : "url(/dots-light.svg)",
+        dots_dark : "url(/dots-dark.svg)",
       }
     },
   },
