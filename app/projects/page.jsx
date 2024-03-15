c82484baee11c9c158f62271051965b8278898e8
PPT1001 <pragash1001@gmail.com>
2024-03-15 23:33:05 +0530

    Project Page Updates

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,22 +1,23 @@
 "use client";
 import React, { useState } from "react";
-import { Tabs } from "@/components/ui/tabs";
+import { Tabs, TabsList, TabsContent, TabsTrigger } from "@/components/ui/tabs";
 import ProjectCard from "@/components/ProjectCard";
 import Link from "next/link";
+import { FiExternalLink } from "react-icons/fi";
 
 const projectData = [
   {
     image: "/work/1.png",
-    category: "react js",
+    category: "FullStack",
     name: "Website 1",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    link: "/",
+    link: "/projects/FoodFlow",
     github: "/",
   },
   {
     image: "/work/2.png",
-    category: "next js",
+    category: "HTML CSS",
     name: "Website 2",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
@@ -25,7 +26,7 @@ const projectData = [
   },
   {
     image: "/work/3.png",
-    category: "HTML js",
+    category: "Java",
     name: "Website 3",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
@@ -34,7 +35,7 @@ const projectData = [
   },
   {
     image: "/work/4.png",
-    category: "Full Stack",
+    category: "Python",
     name: "Website 4",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
@@ -43,15 +44,51 @@ const projectData = [
   },
 ];
 
-const uniqueCategory = [
-  "all projects",
-  ...new Set(projectData.map((item) => item.category)),
+const uniqueCategories = [
+  "All",
+  ...new Set(projectData.map((project) => project.category)),
 ];
 
 const Projects = () => {
+  const [categories, setCategories] = useState(uniqueCategories);
+  const [activeCategory, setActiveCategory] = useState("All");
+  const filteredProjects = projectData.filter((project) => {
+    return activeCategory === "All"
+      ? project
+      : project.category === activeCategory;
+  });
+  console.log(filteredProjects);
+
   return (
     <section className="min-h-screen pt-12">
-      <Link href="/projects/FoodFlow">Hello</Link>
+      <div className="container mx-auto">
+        <h2 className="section-title mb-8 text-center xl:mb-16 mx-auto">
+          Projects
+        </h2>
+        <Tabs defaultValue={activeCategory} className="mb-24 xl:mb-48">
+          <TabsList className="w-full grid h-full md:grid-cols-5 lg:max-w-[640px] mb-12 mx-auto md:border dark:border-none">
+            {categories.map((category, index) => (
+              <TabsTrigger
+                key={index}
+                value={category}
+                className="capitalize w-[162px] md:w-auto"
+                onClick={() => setActiveCategory(category)}
+              >
+                {category}
+              </TabsTrigger>
+            ))}
+          </TabsList>
+          <div className="text-lg xl:mt-8 grid grid-cols-1 lg:grid-cols-3 gap-4">
+            {filteredProjects.map((project, index) => {
+              return (
+                <TabsContent key={index} value={activeCategory}>
+                  <ProjectCard project={project} />
+                </TabsContent>
+              );
+            })}
+          </div>
+        </Tabs>
+      </div>
     </section>
   );
 };
